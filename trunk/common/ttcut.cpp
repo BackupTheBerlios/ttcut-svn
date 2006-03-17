c1ffd8ec99dc7f58d342ad9c38cbfa6885e4a9db
tritime <tritime@7763a927-590e-0410-8f7f-dbc853b76eaa>
2006-03-17 21:03:35 +0000

    version string changed

--- trunk/common/ttcut.cpp
+++ trunk/common/ttcut.cpp
@@ -127,7 +127,7 @@ bool TTCut::logAudioIndexInfo = false;
 // encoder settings
 // --------------------------------------------------------------
 // Version
-QString TTCut::versionString = "TTCut - 0.12.0-preview";
+QString TTCut::versionString = "TTCut - 0.11.2-alpha";
 // Options
 bool TTCut::encoderMode = false;
 
