b04072ed247eeb8f7a8a0823be0acba89b70defa
tritime <tritime@7763a927-590e-0410-8f7f-dbc853b76eaa>
2007-04-30 09:18:36 +0000

    updated to version 0.19.3

--- trunk/gui/ttcutmainwindow.cpp
+++ trunk/gui/ttcutmainwindow.cpp
@@ -779,6 +779,7 @@ void TTCutMainWindow::closeProject()
   {
     videoFileInfo->resetVideoInfo();
     audioFileInfo->clearList();
+    audioList->deleteAll();
     cutListData->deleteAll();
     cutList->clearList();
     streamNavigator->slider()->setValue(0);
